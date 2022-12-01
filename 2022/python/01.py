d079a26d28e0c5beafd45274db5339d25b9f465d
sanamorii <86570135+sanamorii@users.noreply.github.com>
2022-12-01 18:22:50 +0000

    day01-py: completed

---
+++ 2022/python/01.py
@@ -0,0 +1,26 @@
+import sys
+
+elfCalories = []
+calories = 0
+
+
+with open(sys.argv[1], mode="r") as file:
+    data = file.readlines()
+    data = [False if x == '\n' else int(x.rstrip('\n')) for x in data]
+
+### part one
+for i in range(0,len(data)):
+    if not data[i]:
+        elf = (i+1, calories)
+        elfCalories.append(elf)
+        calories = 0
+    calories += data[i]
+
+elfCalories.sort(key=lambda a: a[1], reverse=True)
+print(f"elf: {elfCalories[0][0]} at {elfCalories[0][1]} calories")
+### end
+
+
+### part two
+sumofthree = elfCalories[0][1] + elfCalories[1][1] + elfCalories[2][1]
+print(f"top three: {sumofthree}")
